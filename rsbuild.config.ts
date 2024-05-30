1120623e7f8406d03068e0fd04bc87ca054ccd87
CPatchane <code@patchane.com>
2024-05-30 16:47:02 +0200

    Set up for css order issue

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,14 +1,56 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginSass } from "@rsbuild/plugin-sass"
-
-const testVariable = "#fff"
+import { RspackManifestPlugin } from "rspack-manifest-plugin"
 
 export default defineConfig({
+  source: {
+    entry: {
+      index: "./src/index.ts",
+      styleEntry: [
+        "./src/style.scss",
+        "./node_modules/@drivy/cobalt/utilities.css",
+      ]
+    },
+  },
   plugins: [
     pluginSass({
       sassLoaderOptions: {
-        additionalData: `$__INJECTED_THROUGH_RSPACK__: ${testVariable};`
+        api: "legacy",
       }
     })
-  ]
+  ],
+  performance: {
+    chunkSplit: {
+      override: {
+        cacheGroups: {
+          "cobalt-utilities": {
+            test: /\/node_modules\/@drivy\/cobalt\/utilities/,
+            name: "cobalt-utilities",
+            chunks: "all",
+          },
+        },
+      },
+    },
+  },
+  tools: {
+    rspack: {
+      experiments: {
+        lazyCompilation: false,
+      },
+      plugins: [new RspackManifestPlugin({
+        filename: "manifest.json",
+        writeToDisk: process.env.NODEN_ENV !== "development",
+        generate: (seed, files, entrypoints) => {
+          const manifestFiles = files.reduce((manifest, file) => {
+            manifest[file.name] = file.path
+            return manifest
+          }, seed)
+          return {
+            ...manifestFiles,
+            entrypoints: entrypoints,
+          }
+        },
+      })]
+    }
+  }
 });
